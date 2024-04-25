b30e9bbe46c5b4c42d0a92b2da729d97faf87683
Ben Rehmann <brehmann@me.com>
2024-04-25 18:55:27 -0500

    doors and thanksgiving logic

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,6 +3,9 @@ import React, { useEffect } from 'react';
 import ChristmasDoor from '../Photos/christmasDoor.png';
 import HalloweenDoor from '../Photos/halloweenDoor.png';
 import PatricksDoor from '../Photos/patricksDoor.png';
+import ValentinesDoor from '../Photos/valentineDoor.png';
+import ThanksgivingDoor from '../Photos/thanksgivingDoor.png';
+import IndependenceDoor from '../Photos/independenceDoor.png';
 
 const Header = ({ setHoliday, setHolidate }) => {
 
@@ -15,20 +18,40 @@ const Header = ({ setHoliday, setHolidate }) => {
     //   date: `May 7, ${currentYear} 00:00:00`,
     // },
     {
+      name: 'Valentines Day',
+      date: `February 14, ${currentYear} 00:00:00`,
+      door: ValentinesDoor,
+    },
+    {
       name: 'St. Patricks Day',
       date: `March 17, ${currentYear} 00:00:00`,
       door: PatricksDoor,
     },
-    // christmas throws an error for some reason
     {
-      name: 'Christmas',
-      date: `December 25, ${currentYear} 00:00:00`,
-      door: ChristmasDoor,
+      name: 'Easter',
+      date: `March 17, ${currentYear} 00:00:00`,
+      door: PatricksDoor,
+    },
+    {
+      name: 'Independence Day',
+      date: `July 4, ${currentYear} 00:00:00`,
+      door: IndependenceDoor,
     },
     {
       name: 'Halloween',
       date: `October 31, ${currentYear} 00:00:00`,
       door: HalloweenDoor,
+    },
+    {
+      name: 'Thanksgiving',
+      date: calculateThanksgivingDate(currentYear),
+      door: ThanksgivingDoor,
+    },
+    // ? christmas throws an error for some reason when first
+    {
+      name: 'Christmas',
+      date: `December 25, ${currentYear} 00:00:00`,
+      door: ChristmasDoor,
     },
     // {
     //   name: 'New Years Eve',
@@ -49,8 +72,24 @@ const Header = ({ setHoliday, setHolidate }) => {
     }
   });
 
+  // calculate Thanksgiving - 4th thursday of November
+  function calculateThanksgivingDate(year) {
+    // console.log('THANKSGIVING', year);
+    const november = new Date(year, 10, 1);
+    // console.log('NOV', november);
+    let thanksgivingDate = 1;
+    // console.log('november.getDay()', november.getDay());
+    // find the first thursday
+    while (november.getDay() !== 4) {
+      november.setDate(++thanksgivingDate)
+    }
+    thanksgivingDate += 21; // 21 days after is the 4th thursday
+    console.log('THANKSGIVING', `November ${thanksgivingDate}, ${november.getFullYear()},  00:00:00`);
+    return `November ${thanksgivingDate}, ${november.getFullYear()},  00:00:00`;
+  }
+
   function changeHoliday(newName, newDate) {
-    // console.log('newHoliday', newName, newDate);
+    console.log('newHoliday', newName, newDate);
     setHoliday(newName);
     setHolidate(newDate);
   }
